8996676d69650ec279883d3a6d8c1a59200636f7
GolfTheLegend <god3331tv@gmail.com>
2026-01-20 23:20:47 +0700

    Update Change theme systems

---
+++ daily_record/lib/pages/home_page.dart
@@ -0,0 +1,32 @@
+import 'package:daily_record/components/box.dart';
+import 'package:daily_record/components/button.dart';
+import 'package:daily_record/themes/theme_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Center(
+        child: MyBox(
+          color: Theme.of(context).colorScheme.primary,
+          child: Mybutton(
+            color: Theme.of(context).colorScheme.secondary, 
+            onTap: () {
+              Provider.of<ThemeProvider>(context, listen: false).toggleTheme();
+            }
+            ),
+        ),
+      ),
+    );
+  }
+}
---
+++ daily_record/lib/themes/theme_provider.dart
@@ -0,0 +1,22 @@
+import 'package:daily_record/themes/theme.dart';
+import 'package:flutter/material.dart';
+
+class ThemeProvider extends ChangeNotifier {
+  ThemeData _themeData = lightMode;
+
+  ThemeData get themeData => _themeData;
+
+  set themeData(ThemeData theme) {
+    _themeData = theme;
+    notifyListeners();
+  }
+
+  void toggleTheme (){
+    if(_themeData == lightMode){
+      _themeData = darkMode;
+    } else {
+      _themeData = lightMode;
+    }
+    notifyListeners();
+  }
+}
